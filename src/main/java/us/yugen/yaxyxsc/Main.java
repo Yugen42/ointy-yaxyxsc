88c8d3c9bc7ca1f709103278882531ba786877f1
Andreas Hartmann <andreas.hartmann@conrad.de>
2018-05-25 23:49:22 +0200

    yo

--- src/main/java/us/yugen/yaxyxsc/Main.java
+++ src/main/java/us/yugen/yaxyxsc/Main.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         MockData.mockData();
         SpringApplication.run(MainController.class, args);
     }
