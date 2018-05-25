437fd08a0eaa9e05fbaf535983c6c3ad520ea61d
CLUBMATEjks <matejka-lukas@hotmail.de>
2018-05-25 23:19:51 +0200

    Commit DataStore

---
+++ src/main/java/us/yugen/yaxyxsc/DataStore.java
@@ -0,0 +1,12 @@
+package us.yugen.yaxyxsc;
+
+import us.yugen.yaxyxsc.entities.BuyingList;
+import us.yugen.yaxyxsc.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataStore {
+    public static final List<BuyingList> buyingLists = new ArrayList<>();
+    public static final List<User> user = new ArrayList<>();
+}
