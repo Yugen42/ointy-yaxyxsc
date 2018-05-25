5f6944941cdf9a6387b297eefeb6cd67b397d3a4
Andreas Hartmann <andreas.hartmann@conrad.de>
2018-05-25 23:15:33 +0200

    oh, ok.

---
+++ src/main/java/us/yugen/yaxyxsc/entities/Address.java
@@ -0,0 +1,13 @@
+package us.yugen.yaxyxsc.entities;
+
+/**
+ * @author Andreas Hartmann
+ */
+public class Address {
+    public int x;
+    public int y;
+    public int zip;
+    public String city;
+    public String street;
+    public int houseNumber;
+}
--- src/main/java/us/yugen/yaxyxsc/entities/User.java
+++ src/main/java/us/yugen/yaxyxsc/entities/User.java
@@ -4,4 +4,6 @@ package us.yugen.yaxyxsc.entities;
  * @author Andreas Hartmann
  */
 public class User {
+    public String firstName;
+    public String lastName;
 }
