a99ceb01774d030d5d08d5431c6f0a68717a05bf
Andreas Hartmann <andreas.hartmann@conrad.de>
2018-05-25 22:55:25 +0200

    oh, ok.

---
+++ src/main/java/us/yugen/yaxyxsc/BuyingList.java
@@ -0,0 +1,11 @@
+package us.yugen.yaxyxsc;
+
+import java.util.List;
+
+/**
+ * @author Andreas Hartmann
+ */
+public class BuyingList {
+    int x, y;
+    List<String> items;
+}
--- src/main/java/us/yugen/yaxyxsc/MainController.java
+++ src/main/java/us/yugen/yaxyxsc/MainController.java
@@ -5,13 +5,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
 public class MainController {
 
-    @RequestMapping("/")
+    @RequestMapping("/getBuyingLists")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        List<BuyingList> allLists = new ArrayList<>();
+
+        BuyingList testListA = new BuyingList();
+        List<String> itemList = new ArrayList<>();
+        itemList.add("Condoms XXS");
+        itemList.add("tampons");
+        testListA.items = itemList;
+
+        allLists.add(testListA);
+        return "all";
     }
 }
